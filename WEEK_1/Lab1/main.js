2d5cbf470269bf058c17ca3ea5a19b1afca87a38
Addisalem <addis@coder.com>
2021-06-22 00:05:56 -0500

    w1lab1

---
+++ WEEK_1/Lab1/main.js
@@ -0,0 +1,9 @@
+const even = require("exercise1")
+const odd = require("exercise1")
+
+console.log([1, 2, 3, 4, 5, 6, 7, 8].even()); // [2,4,6,8]
+ar = [1, 2, 3, 4, 5, 6, 7, 8]
+let temp = ar.odd()
+// console.log([1, 2, 3, 4, 5, 6, 7, 8].odd()); // [1,3,5,7]
+console.log(temp)
+
